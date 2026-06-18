7ca84b90348603d6946b5ab84815f58eb6b63be9
Riya Yadav <2005riyadav@gmail.com>
2026-06-18 22:36:45 +0530

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+const socket = io();  // connection req goes to backend..
+
+if(navigator.geolocation){// function for positon
+    navigator.geolocation.watchPosition((position)=>{
+        const {latitude, longitude} =position.coords
+        socket.emit('send-location', {latitude, longitude})
+    }, (error)=>{
+        console.log(error)
+    },
+    {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge:0//casition
+    }
+)
+}
+
+
+const map=L.map("map").setView([0,0], 16)
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "OpenStreetMap"
+}).addTo(map)
+
+const markers={}
+
+socket.on('receive-location', (data)=>{
+    const {id, latitude, longitude} = data
+    map.setView([latitude, longitude]);
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude])
+    }else{
+        markers[id]= L.marker([latitude,longitude]).addTo(map)
+    }
+})
+
+socket.on("user-disconnect", (id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id])
+        delete markers[id]
+    }
+
+})
